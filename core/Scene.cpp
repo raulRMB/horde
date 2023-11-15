927939c8240c067488e6770e60a420879e9d2277
raulRMB <92171903+raulRMB@users.noreply.github.com>
2023-11-15 08:59:00 -0500

    Initial commit

---
+++ core/Scene.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Raul Romero on 2023-11-15.
+//
+
+#include "Scene.h"
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <raylib.h>
+#include <glm/glm.hpp>
+#include <entt/entt.hpp>
+#include <enet/enet.h>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
