a3f9f45f0d3162f8fa6389bb7956beeb7d28635b
Mike Ashcroft <MashDevel@users.noreply.github.com>
2023-12-06 21:32:21 -0500

    center healthbars

--- core/systems/moba/AttributeUI.cpp
+++ core/systems/moba/AttributeUI.cpp
@@ -24,14 +24,16 @@ void SAttributeUI::Draw2D()
     {
         CTransform& t = registry.get<CTransform>(entity);
         raylib::Vector2 healthBarPos = GetWorldToScreen(
-                (raylib::Vector3){ t.Position.x, t.Position.y + 10.0f, t.Position.z },
+                (raylib::Vector3){ t.Position.x, t.Position.y + 12.0f, t.Position.z },
                 ToRaylibCamera(Game::Instance().GetActiveCamera()));
         CAttributes& ac = registry.get<CAttributes>(entity);
         FAttribute& health = *Util::GetAttribute(ac, "health");
         if (health.id != "empty") {
-            raylib::DrawRectangle(healthBarPos.x, healthBarPos.y, 80, 10, raylib::GRAY);
-            raylib::DrawRectangle(healthBarPos.x, healthBarPos.y, 80 * health.get()/health.max, 10, raylib::GREEN);
-            raylib::DrawTextEx(font, "Username", raylib::Vector2{healthBarPos.x, healthBarPos.y - 13}, 13, 1, raylib::WHITE);
+            int sizeX = 80;
+            int sizeXh = 40;
+            raylib::DrawRectangle(healthBarPos.x - sizeXh, healthBarPos.y, sizeX, 10, raylib::GRAY);
+            raylib::DrawRectangle(healthBarPos.x - sizeXh, healthBarPos.y, sizeX * health.get()/health.max, 10, raylib::GREEN);
+            raylib::DrawTextEx(font, "Username", raylib::Vector2{healthBarPos.x - sizeXh, healthBarPos.y - 13}, 13, 1, raylib::WHITE);
         }
     }
 }
