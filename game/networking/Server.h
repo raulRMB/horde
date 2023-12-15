f2039336e9103d0ba914be0ec6221142db25067c
Mike Ashcroft <MashDevel@users.noreply.github.com>
2023-12-15 14:02:32 -0500

    generic spawn entity network msg

--- game/networking/Server.h
+++ game/networking/Server.h
@@ -31,6 +31,7 @@ public:
     void Sync(entt::entity e, CTransform& t, std::vector<ENetPeer*> c);
     void Sync(entt::entity e, struct CAttributeSet& ac, std::vector<ENetPeer*> c);
     void Sync(entt::entity e, struct CCharacterAnimation& ca, std::vector<ENetPeer*> c);
+    void SendSpawnEntity(uint32_t netId, uint32_t entityType, CTransform& t);
 private:
     void Send(flatbuffers::FlatBufferBuilder &builder, Net::Events type, flatbuffers::Offset<> data, std::vector<ENetPeer*>& c,
               ENetPacketFlag flag);
